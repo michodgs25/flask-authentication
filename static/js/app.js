0deb851f800721a999bc512cd355a0d7da420d4d
michodgs25 <michodgs@gmail.com>
2021-01-15 23:54:40 +0000

    Add confirm password js

--- static/js/app.js
+++ static/js/app.js
@@ -6,3 +6,17 @@ $(document).ready(function () {
         $('.sidenav').sidenav();
     });
 });
+
+var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;

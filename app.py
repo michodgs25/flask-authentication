74337160514ce735ca4fff7bd035f65916a3d134
michodgs25 <michodgs@gmail.com>
2021-01-15 19:09:51 +0000

    Add boilerplate cards to home

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
